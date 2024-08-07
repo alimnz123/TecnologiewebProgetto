2fa82bd9913204fd62999057b02b6f185bc99e5a
aliceMenzani <285278@studenti.unimore.it>
2024-08-07 18:38:10 +0200

    58TH COMMIT - ADMIN PANEL

--- GestionaleCondominio/Condominio/admin.py
+++ GestionaleCondominio/Condominio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DocumentiPalazzo, Verbale, Lettera_Convocazione
 
 # Register your models here.
+admin.site.register(DocumentiPalazzo)
+admin.site.register(Verbale)
+admin.site.register(Lettera_Convocazione)
